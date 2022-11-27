89521fb89fb1c35ff604bcf40a91393158f51cd2
KangSDhi <kangteknisi@gmail.com>
2022-11-27 18:15:47 +0700

    Tipe Data Character

---
+++ src/TipeDataChar.java
@@ -0,0 +1,11 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char S = 'S';
+        char I = 'I';
+        char G = 'G';
+
+        System.out.println(S);
+        System.out.println(I);
+        System.out.println(G);
+    }
+}
