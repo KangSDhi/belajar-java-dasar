95f739baeff6b205be916c9c755def30d58d3be2
KangSDhi <kangteknisi@gmail.com>
2022-11-28 09:43:55 +0700

    Expression, Statement dan Block

---
+++ src/Block.java
@@ -0,0 +1,15 @@
+public class Block {
+    // Block {}
+    public static void main(String[] args) {
+        // Expression hello_world = "Hello World"
+        var hello_world = "Hello World";
+
+        //Statement System.out.println(hello_world);
+        System.out.println(hello_world);
+
+        // Block {}
+        {
+            System.out.println(hello_world);
+        }
+    }
+}
